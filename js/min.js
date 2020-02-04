1c5aad557d83b4b9a0ac0adc5c2e10172dab4988
StanD01 <gameplayer140701@gmail.com>
2020-02-04 17:17:18 +0500

    click

---
+++ js/min.js
@@ -0,0 +1,8 @@
+window.onload = function (){
+    var click = this.document.querySelector("h1");
+    var change = this.document.querySelector('h1');
+
+    click.addEventListener('click', function () {
+        change.style.color = 'red';
+    })
+}
